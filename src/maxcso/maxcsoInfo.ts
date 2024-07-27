21d159bc59f3129a9f312acd3b31ed8cb24358ca
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-07-27 09:08:59 -0700

    Feature: function to get the uncompressed file size (#6)

--- src/maxcso/maxcsoInfo.ts
+++ src/maxcso/maxcsoInfo.ts
@@ -1,6 +1,10 @@
 import MaxcsoBin from './maxcsoBin.js';
 
 export default {
+  /**
+   * Given a compressed CSO/ZSO/DAX file, get its uncompressed ISO's CRC32. This is fast/cheap to
+   * calculate.
+   */
   async uncompressedCrc32(inputFilename: string): Promise<string> {
     const output = await MaxcsoBin.run(['--crc', inputFilename]);
     const crcMatch = output.replace(/[\n\r]/g, '').match(/ ([\da-f]{8})$/);
@@ -8,5 +12,18 @@ export default {
       throw new Error('failed to get CRC of uncompressed file');
     }
     return crcMatch[1];
+  },
+
+  /**
+   * Given a compressed CSO/ZSO/DAX file, get its uncompressed ISO's filesize. This is expensive
+   * to calculate as full decompression is necessary.
+   */
+  async uncompressedSize(inputFilename: string): Promise<number> {
+    const output = await MaxcsoBin.run(['--measure', inputFilename]);
+    const sizeMatch = output.match(/(\d+) -> \d+/);
+    if (sizeMatch === null) {
+      throw new Error('failed to get size of uncompressed file');
+    }
+    return Number.parseInt(sizeMatch[1], 10);
   },
 };
