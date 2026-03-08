f499d71f50bc8e11c5ece1e96a7cde537e4343a1
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-03-07 18:30:12 -0800

    Feature: use Bun-bundled binaries

--- packages/maxcso/src/maxcso/maxcsoBin.ts
+++ packages/maxcso/src/maxcso/maxcsoBin.ts
@@ -1,7 +1,12 @@
 import { promisify } from 'node:util';
 import fs from 'node:fs';
 import which from 'which';
-import * as child_process from 'node:child_process';
+import child_process from 'node:child_process';
+import { Mutex } from 'async-mutex';
+import path from 'node:path';
+import stream from 'node:stream';
+import os from 'node:os';
+import crypto from 'node:crypto';
 
 export interface MaxcsoBinOptions {
   binaryPreference?: MaxcsoBinaryPreference
@@ -19,6 +24,8 @@ export enum MaxcsoBinaryPreference {
 export default class MaxcsoBin {
   private static MAXCSO_BIN: string | undefined;
 
+  private static readonly MAXCSO_BIN_MUTEX = new Mutex();
+
   private static async getBinPath(
     binaryPreference?: MaxcsoBinaryPreference,
   ): Promise<string | undefined> {
@@ -26,29 +33,121 @@ export default class MaxcsoBin {
       return this.MAXCSO_BIN;
     }
 
-    if ((binaryPreference ?? MaxcsoBinaryPreference.PREFER_BUNDLED_BINARY)
-      === MaxcsoBinaryPreference.PREFER_BUNDLED_BINARY
-    ) {
-      const pathBundled = await this.getBinPathBundled();
-      this.MAXCSO_BIN = pathBundled ?? (await this.getBinPathExisting());
-    } else {
-      const pathExisting = await this.getBinPathExisting();
-      this.MAXCSO_BIN = pathExisting ?? (await this.getBinPathBundled());
-    }
+    return this.MAXCSO_BIN_MUTEX.runExclusive(async () => {
+      if (this.MAXCSO_BIN) {
+        return this.MAXCSO_BIN;
+      }
 
-    return this.MAXCSO_BIN;
+      if ((binaryPreference ?? MaxcsoBinaryPreference.PREFER_BUNDLED_BINARY)
+      === MaxcsoBinaryPreference.PREFER_BUNDLED_BINARY
+      ) {
+        const pathBundled = await this.getBinPathBundled();
+        this.MAXCSO_BIN = pathBundled ?? (await this.getBinPathExisting());
+      } else {
+        const pathExisting = await this.getBinPathExisting();
+        this.MAXCSO_BIN = pathExisting ?? (await this.getBinPathBundled());
+      }
+
+      return this.MAXCSO_BIN;
+    });
   }
 
   private static async getBinPathBundled(): Promise<string | undefined> {
-    // try {
-    const maxcso = await import(`@emmercm/maxcso-${process.platform}-${process.arch}`);
-    const prebuilt = maxcso.default;
-    if (await promisify(fs.exists)(prebuilt)) {
-      return prebuilt;
+    const bunPath = await this.getBinPathBundledBun();
+    if (bunPath !== undefined) {
+      return bunPath;
     }
-    // } catch { /* ignored */ }
+
+    try {
+      const maxcso = await import(`@emmercm/maxcso-${process.platform}-${process.arch}`);
+      const prebuilt = maxcso.default;
+      try {
+        await promisify(fs.stat)(prebuilt);
+        return prebuilt;
+      } catch { /* ignored */ }
+    } catch { /* ignored */ }
 
     return undefined;
+  }
+
+  /**
+   * Look for maxcso binaries bundled with:
+   * `bun build --compile --asset-naming="[name].[ext]" maxcso *.dylib`
+   */
+  private static async getBinPathBundledBun(): Promise<string | undefined> {
+    try {
+      const { embeddedFiles } = await import('bun');
+
+      // Find all files that might be maxcso-related
+      const maxcsoBlob = embeddedFiles.find((blob) => {
+        // @ts-expect-error https://github.com/oven-sh/bun/issues/20700
+        const blobName: string = blob.name;
+        return blobName.toLowerCase().startsWith('maxcso');
+      });
+
+      if (maxcsoBlob !== undefined) {
+        // Create the temporary directory
+        const hash = crypto.createHash('md5');
+        await maxcsoBlob.stream().pipeTo(new WritableStream({
+          write(chunk): void {
+            hash.update(chunk);
+          },
+        }));
+        const temporaryDirectory = await this.getTemporaryDirectory(`maxcso-${hash.digest('hex').slice(0, 7)}`);
+
+        // Find additional files that might be necessary
+        const dylibBlobs = embeddedFiles.filter((blob) => {
+          // @ts-expect-error https://github.com/oven-sh/bun/issues/20700
+          const blobName: string = blob.name;
+          return blobName.toLowerCase().endsWith('.dylib');
+        });
+
+        // Extract all files if necessary
+        const temporaryBlobs = await Promise.all([maxcsoBlob, ...dylibBlobs].map(async (blob) => {
+          // @ts-expect-error https://github.com/oven-sh/bun/issues/20700
+          const blobName: string = blob.name.replace(/-[\da-z]{8}\./, '.').replace(/\.+$/, '');
+          const temporaryBlob = path.join(temporaryDirectory, blobName);
+          try {
+            await promisify(fs.stat)(temporaryBlob);
+            return temporaryBlob;
+          } catch {
+            /* ignored */
+          }
+          const writableStream = stream.Writable.toWeb(fs.createWriteStream(temporaryBlob));
+          await blob.stream().pipeTo(writableStream);
+          await promisify(fs.chmod)(temporaryBlob, 0o755); // chmod +x
+          return temporaryBlob;
+        }));
+        return temporaryBlobs.find((temporaryBlob) => path.basename(temporaryBlob).startsWith('maxcso'));
+      }
+    } catch { /* ignored */ }
+
+    return undefined;
+  }
+
+  private static async getTemporaryDirectory(temporaryDirectoryBasename: string): Promise<string> {
+    const candidateDirectories = [
+      path.join(os.tmpdir(), temporaryDirectoryBasename),
+      path.join(process.cwd(), '.maxcso', temporaryDirectoryBasename),
+      path.join(os.homedir(), '.maxcso', temporaryDirectoryBasename),
+    ];
+    /* eslint-disable no-await-in-loop */
+    for (const candidateDirectory of candidateDirectories) {
+      try {
+        try {
+          await promisify(fs.stat)(candidateDirectory);
+        } catch {
+          await promisify(fs.mkdir)(candidateDirectory, { recursive: true });
+        }
+        const temporaryFile = path.join(candidateDirectory, temporaryDirectoryBasename);
+        await promisify(fs.writeFile)(temporaryFile, temporaryFile);
+        await promisify(fs.unlink)(temporaryFile);
+        return candidateDirectory;
+      } catch {
+        /* ignored */
+      }
+    }
+    throw new Error("couldn't find a suitable temporary directory");
   }
 
   private static async getBinPathExisting(): Promise<string | undefined> {
